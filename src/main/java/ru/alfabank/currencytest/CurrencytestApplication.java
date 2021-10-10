fe3e8759d310747068fc1e10a5b1b96b1e9c7143
c0dered273 <okaschuk@gmail.com>
2021-10-10 16:58:26 +0300

    Initial commit

---
+++ src/main/java/ru/alfabank/currencytest/CurrencytestApplication.java
@@ -0,0 +1,13 @@
+package ru.alfabank.currencytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alfabank/currencytest/CurrencytestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alfabank.currencytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
